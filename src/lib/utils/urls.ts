f31506aa5878dc00c0d729f1b93ecfbde2850f1b
git <stian.larsen@sparebank1.no>
2025-08-28 17:20:26 +0200

    Securing API key behind .env (too late tho...)

--- src/lib/utils/urls.ts
+++ src/lib/utils/urls.ts
@@ -1,2 +1,1 @@
-export const BITCOIN_PRICE_DETAILS_API_URL =
-  "https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=100&api_key=8ae55d463e1bf8d38b4a502ca47512f9b1dec21533ad9af7acb993e8ba952bc2";
+export const BITCOIN_PRICE_DETAILS_API_URL = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=100&api_key=${process.env.NEXT_PUBLIC_BITCOIN_API_KEY}`;
